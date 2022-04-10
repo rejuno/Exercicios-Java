d035c567501b0965d385d080a2e25eefc908893e
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-04-10 02:17:48 -0300

    Add files via upload

---
+++ ex4.java
@@ -0,0 +1,34 @@
+package projeto;
+import java.util.Scanner;
+public class ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				Scanner ler = new Scanner(System.in);
+				System.out.println("Digite um numero: ");
+				int num = ler.nextInt();
+				
+				if(num > 0 ) {
+					System.out.println("Positivo");
+					
+				}
+				else {
+					System.out.println("Negativo");
+					
+				}
+				
+				if(num%3 == 0) {
+					System.out.println("Multiplo de 3");
+					
+				} else {
+					System.out.println("Nao e multiplo de 3");
+					
+				}
+			}
+		
+
+
+	}
+
+
